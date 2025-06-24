c99fdf20fc20c4f986ce9738e57f15eec65d246a
xconvics <93058308+xconvics@users.noreply.github.com>
2025-06-24 21:09:47 +0200

    VAE CNN for classification (#5)
    * checkpoint; vae reconstruction started to look ok, sampling bad, ddpm maybe? but still bad
    
    * add dataset for classification with piano roll

--- src/dataloader/dataset.py
+++ src/dataloader/dataset.py
@@ -7,11 +7,19 @@ import torch
 from torch.utils.data import Dataset
 import tqdm
 
-from src.dataloader.midi_parser import parse_midi_file_to_sparse, parse_midi_file_to_pitch_start_end_tensor
+from src.dataloader.midi_parser import parse_midi_file_to_sparse, parse_midi_file_to_pitch_start_end_tensor  
 
 
 class PianoRollMidiDataset(Dataset):
-    def __init__(self, midi_files: list[str], frame_per_second: int = 64, window_len=10, verbose: bool = False, strip_bounds: bool = True, pitch_to_strip: Optional[tuple] = None):
+    def __init__(
+            self,
+            midi_files: list[str], 
+            frame_per_second: int = 64, 
+            window_len=10, 
+            verbose: bool = False, 
+            strip_bounds: bool = True, 
+            pitch_to_strip: Optional[tuple] = None
+        ):
         """
         Initializes the MidiDataset with a directory containing MIDI files.
         Splits it into time windows with specified duration and frame rate.
@@ -67,8 +75,6 @@ class PianoRollMidiDataset(Dataset):
             for i in range(0, total_rows - window_size + 1, window_size)
         ]
 
-
-
     def _init_midi_tensors(self, midi_files: list[str]):
         """
         Initializes the MIDI tensors by parsing each MIDI file and converting it to a tensor representation.
@@ -103,6 +109,84 @@ class PianoRollMidiDataset(Dataset):
         return tensor
 
 
+
+class PianoRollClassificationDataset(PianoRollMidiDataset):
+    def __init__(
+            self,
+            midi_files: list[str], 
+            frame_per_second: int = 64, 
+            window_len=10, 
+            verbose: bool = False, 
+            strip_bounds: bool = True, 
+            pitch_to_strip: Optional[tuple] = None
+        ):
+        self.labels_mapping = {}
+        self.labels = torch.tensor([], dtype=torch.int64)
+
+        super().__init__(midi_files, frame_per_second, window_len, verbose, strip_bounds, pitch_to_strip)
+
+        
+        
+    def _init_midi_tensors(self, midi_files: list[str]):
+        """
+        Initializes the MIDI tensors by parsing each MIDI file and converting it to a tensor representation.
+        """
+        composers = []
+        iter_ = tqdm.tqdm(midi_files) if self.verbose else midi_files
+        for midi_file in iter_:
+            midi_sparse = parse_midi_file_to_sparse(
+                midi_file,
+                frame_per_second=self._fps,
+                strip_bounds=self.strip_bounds,
+                strip_pitch=self.strip_frequencies
+            )
+            if midi_sparse is None or midi_sparse.shape[0] == 0:
+                if self.verbose:
+                    print(f"Warning: MIDI file {midi_file} could not be parsed or is empty.")
+            else:
+                splited_midi = self._split_into_time_windows(midi_sparse)
+                self.midi_sparse_list.extend(splited_midi)
+                
+                composer = os.path.basename(midi_file).split(',')[0]
+                composers.extend([composer] * len(splited_midi))
+
+        # Encode composers to integers
+        unique_composers = sorted(set(composers))
+        self.labels_mapping = {idx: composer for idx, composer in enumerate(unique_composers)}
+        self.labels = torch.tensor([unique_composers.index(c) for c in composers], dtype=torch.int64)
+
+        assert len(self.labels) == len(self.midi_sparse_list), "Labels length must match MIDI tensors length."
+        
+        if self.verbose:
+            print(f"Initialized {len(self.midi_sparse_list)} MIDI tensors with {len(unique_composers)} unique composers.")
+
+    @lru_cache(maxsize=1024)
+    def __getitem__(self, idx):
+        if idx < 0 or idx >= len(self.midi_sparse_list):
+            raise IndexError("Index out of range.")
+        midi_sparse =  self.midi_sparse_list[idx]
+        tensor = torch.tensor(midi_sparse.toarray(), dtype=torch.float32)
+        label = self.labels[idx]
+        return tensor, label
+    
+    def get_composer_name(self, idx: int) -> Optional[str]:
+        """
+        Returns the composer name for a given index.
+
+        :param idx: Index of the MIDI tensor.
+        :return: Composer name or None if index is out of range.
+        """
+        if idx < 0 or idx >= len(self.labels_mapping):
+            return None
+        return self.labels_mapping[idx]
+    
+    def get_mapping(self) -> dict[int, str]:
+        """
+        Returns the mapping of labels to composer names.
+
+        :return: Dictionary mapping label indices to composer names.
+        """
+        return self.labels_mapping
 
 
 class EventMidiDataset(Dataset):
